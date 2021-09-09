9706d4670c2b8feadfcb9f4639446a16c6a56a28
vcrolack <victorolac@gmail.com>
2021-09-09 14:27:15 -0300

    porsi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,46 @@ import {HomeComponent} from './home/home.component';
 import {ProductsComponent} from './products/products.component';
 import {ContactComponent} from './contact/contact.component';
 import {DemoComponent} from './demo/demo.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {LayoutComponent} from './layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'products',
-    component: ProductsComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      }
+    ]
   },
   {
     path: 'demo',
     component: DemoComponent
-  }
+  },
+  {
+    path: '**',
+    component:PageNotFoundComponent
+  },
 ];
 
 @NgModule({
@@ -30,3 +52,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+
+import {Product} from '../product.model';
+import {ProductsService} from '../services/products/products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.product = this.productsService.getProduct(id);
+    });
+  }
+
+}
